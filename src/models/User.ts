26cc8b51c4a75ad660a992f4225cb10a8c5051e5
david11yf29 <david11yf29@gmail.com>
2021-01-14 13:47:55 +0800

    updated

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
 interface UserProps {
@@ -19,25 +18,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data)
-      })
-  };
-
-  save(): void {
-    
-    const id = this.get('id');
-
-    if (this.get('id')) {
-      // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      // post
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
